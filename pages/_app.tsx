53b4c496a8254dfb27ef1553e60efed6ab4e6cb0
matthew.cobbing <matthew.cobbing@perkbox.com>
2021-02-10 23:48:03 +0000

    add social media preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,19 @@ const MyApp = ({ Component, pageProps }) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <meta property="og:url" content="https://cobbing.dev" key="ogurl" />
+        <meta
+          property="og:image"
+          content="https://images.ctfassets.net/xcavdzt870rr/69KVWlfZcObV6j13OU2wB5/1b7a08968880e278474aa6af5b6a9f41/avataaars.png"
+          key="ogimage"
+        />
+        <meta property="og:site_name" content="cobbing.dev" key="ogsitename" />
+        <meta property="og:title" content="cobbing.dev" key="ogtitle" />
+        <meta
+          property="og:description"
+          content="Matthew Cobbing's Portfolio Website"
+          key="ogdesc"
+        />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
